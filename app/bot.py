3ca2008bad9d24c46400f4122019b8da7d112653
dazzzed <petumit@yandex.ru>
2021-11-09 10:35:23 +0300

    initial bot setup

--- app/bot.py
+++ app/bot.py
@@ -1,18 +1,34 @@
 from fastapi import FastAPI, Request
-from telebot import TeleBot
+import telebot
 from app.config import BOT_TOKEN
+import uvicorn
 
 app = FastAPI()
-bot = TeleBot(BOT_TOKEN)
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.reply_to(message, "Hello, " + message.from_user.first_name)
+
+
+@bot.mesage_handler(func=lambda message: True, content_types=["text"])
+def echo(message):
+    bot.reply_to(message, message.text)
 
 
 @app.get("/")
 def webhook():
-    bot.remove_webhook()
     bot.set_webhook()
 
 
+@app.post("/"+BOT_TOKEN, status_code=200)
+async def get_message(request: Request):
+    data = await request.json()
+    update = telebot.types.Update.de_json(data)
+    bot.process_updates([update])
+    return {"message_created": str(update)}
 
-@app.post("/")
-def get_message(request: Request):
-    pass
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- app/config.py
+++ app/config.py
@@ -1,1 +1,2 @@
 BOT_TOKEN = "2146860889:AAGNYkxnI9IwQDr0sRVtDdbi55T4Hc9Ro_I"
+APP_URL = "https://balaboba-telegram-bot.herokuapp.com/"
